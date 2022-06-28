2c2d56e80bc6f6515c6bcc272bd51daae9d11f8a
angel <89686990+an0nangel@users.noreply.github.com>
2022-06-28 10:46:48 +0200

    securing things before aredesign

--- components/banner/banner.jsx
+++ components/banner/banner.jsx
@@ -6,6 +6,12 @@ export default function Banner() {
       <img src="https://4.bp.blogspot.com/-1hBZaDQV6lY/XD930XvmeRI/AAAAAAAAAuM/Mb0nonSZOFkk0umjU7mXMdMhroDjvTq0ACKgBGAs/w0/bitcoin-cryptocurrency-cube-abstract-4-4k.jpg"
            width="100%"
            class={styles.img}/>
+           
+      <div class={styles.header}>
+      </div>
+      <h1 id={styles.text}>Join the world's first <br />
+       Crypto workplace!</h1>
+
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class={styles.curve}>
         <path fill="#fff" fill-opacity="1" d="M0,256L80,229.3C160,203,320,149,480,144C640,139,800,181,960,202.7C1120,224,1280,224,1360,224L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
       </svg>
--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,5 +1,6 @@
 import styles from './Navbar.module.css'
 
+
 export default function Navbar() {
   return(
     <>
@@ -18,6 +19,20 @@ export default function Navbar() {
 
         <section class={styles.start}>
           <a href=".">Get Started</a>
+        </section>
+
+        <section class={styles.menu}>
+          <button class={styles.menu_button}>
+            <img src="https://vectorified.com/images/hamburger-nav-icon-9.png" width="44px" />
+          </button>
+
+          <div class={styles.dropdown}>
+            <ul class={styles.drop_options}>
+              <a href="."><li class={styles.options}>Log In</li></a>
+              <a href="."><li class={styles.options}>Sign Up</li></a>
+              <a href="."><li class={styles.options}>Help</li></a>
+            </ul>
+          </div>
         </section>
       </nav>
     </>
