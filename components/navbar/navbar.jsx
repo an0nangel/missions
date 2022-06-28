b45bc3f6d7700b797780f5ae876e804df4413fb9
angel <89686990+an0nangel@users.noreply.github.com>
2022-06-28 12:26:19 +0200

    Redesigned Navbar (not responsive)

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,40 +1,22 @@
-import styles from './Navbar.module.css'
+import styles from './Navbar.module.css';
 
+export default function Navbar(props) {
+  return (
+    <nav className={styles.nav}>
+      <section className={styles.opt_container}>
+        <a href="/"><img src="/logo.png" alt="logo" className={styles.logo} /></a>
 
-export default function Navbar() {
-  return(
-    <>
-      <nav class={styles.container}>
-        <section class={styles.logo_container}>
-          <a href="."><p class={styles.logo}>Missions</p></a>
-        </section>
+        <ul className={styles.nav_list}>
+          <li className={styles.nav_item}><a href=".">Explore</a></li>
+          <li className={styles.nav_item}><a href=".">Log In</a></li>
+          <li className={styles.nav_item}><a href=".">Sign Up</a></li>
+          <li className={styles.nav_item}><a href=".">Help</a></li>
+        </ul>
+      </section>
 
-        <section class={styles.options_container}>
-          <ul class={styles.options_list}>
-            <a href="."><li class={styles.options}>Log In</li></a>
-            <a href="."><li class={styles.options}>Sign Up</li></a>
-            <a href="."><li class={styles.options}>Help</li></a>
-          </ul>
-        </section>
-
-        <section class={styles.start}>
-          <a href=".">Get Started</a>
-        </section>
-
-        <section class={styles.menu}>
-          <button class={styles.menu_button}>
-            <img src="https://vectorified.com/images/hamburger-nav-icon-9.png" width="44px" />
-          </button>
-
-          <div class={styles.dropdown}>
-            <ul class={styles.drop_options}>
-              <a href="."><li class={styles.options}>Log In</li></a>
-              <a href="."><li class={styles.options}>Sign Up</li></a>
-              <a href="."><li class={styles.options}>Help</li></a>
-            </ul>
-          </div>
-        </section>
-      </nav>
-    </>
+      <button className={styles.start_button}>
+        Start Now
+      </button>
+    </nav>
   );
 }
--- pages/home.jsx
+++ pages/home.jsx
@@ -11,9 +11,9 @@ export default function Home() {
       <Head>
         <title>Missions | Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <Banner />
 
       <Layout>
         <Title><p class={styles.darkBlue}>Start Working Now</p></Title>
