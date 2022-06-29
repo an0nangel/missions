21a37245088a3a0761d94c87fbddb95f4d07c340
angel <89686990+an0nangel@users.noreply.github.com>
2022-06-29 10:26:35 +0200

    Mobile working navbar but still incomplete!

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,22 +1,50 @@
+import React from 'react';
 import styles from './Navbar.module.css';
 
-export default function Navbar(props) {
-  return (
-    <nav className={styles.nav}>
-      <section className={styles.opt_container}>
-        <a href="/"><img src="/logo.png" alt="logo" className={styles.logo} /></a>
 
-        <ul className={styles.nav_list}>
+function handleClick(state, setState) {
+  setState(!state);
+}
+
+
+export default function Navbar(props) {
+  const [dropdown, setDropdown] = React.useState(false);
+
+  return (
+    <header>
+      <section className={styles.mobile_container} id={dropdown ? styles.show : null}>
+        <ul className={styles.mnav_list}>
           <li className={styles.nav_item}><a href=".">Explore</a></li>
           <li className={styles.nav_item}><a href=".">Log In</a></li>
           <li className={styles.nav_item}><a href=".">Sign Up</a></li>
           <li className={styles.nav_item}><a href=".">Help</a></li>
         </ul>
+
+        <button className={styles.start_button}>
+          Start Now
+        </button>
       </section>
 
-      <button className={styles.start_button}>
-        Start Now
-      </button>
-    </nav>
+      <nav className={styles.nav}>
+        <section className={styles.opt_container}>
+          <a href="/"><img src="/logo.png" alt="logo" className={styles.logo} /></a>
+
+          <ul className={styles.nav_list}>
+            <li className={styles.nav_item}><a href=".">Explore</a></li>
+            <li className={styles.nav_item}><a href=".">Log In</a></li>
+            <li className={styles.nav_item}><a href=".">Sign Up</a></li>
+            <li className={styles.nav_item}><a href=".">Help</a></li>
+          </ul>
+        </section>
+
+        <button className={styles.menu_button} onClick={() => handleClick(dropdown, setDropdown)}>
+          <img src="/menu.png" alt="menu" className={styles.menu_icon} />
+        </button>
+
+        <button className={styles.start_button}>
+          Start Now
+        </button>
+      </nav>
+    </header>
   );
 }
