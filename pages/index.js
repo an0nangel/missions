ca1c1f7ab9ca232f57a10f7ff4d348ac4257f731
angel <89686990+an0nangel@users.noreply.github.com>
2022-07-05 20:54:59 +0200

    minor fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Home from './home.jsx';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Index() {
   return (
     <Home />
   );
