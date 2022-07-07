47d9f191b846170cfd20256e96509222133f16d1
angel <89686990+an0nangel@users.noreply.github.com>
2022-07-07 10:48:33 +0200

    add: Navbar component
    The Navbar may recieve changes in the future!

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Navbar from '../components/navbar/navbar.jsx'
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -7,8 +8,10 @@ export default function Home() {
       <Head>
         <title>Missions | Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
+
+      <Navbar />
     </>
   );
 }
