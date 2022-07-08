6451877802c8369e98c1a002b43b6bfe5a6fb761
angel <89686990+an0nangel@users.noreply.github.com>
2022-07-08 11:30:19 +0200

    add: banner
    Addition of an incomplete banner that will be left like this until the theme is figured out better!

---
+++ components/banner/banner.jsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import styles from './Banner.module.css'
+
+const message = "Feel free to start earning money online!";
+const description = "Looking forward to work online? Missions is the perfect platform for that. We provide you a wide range of missions that you can take on at any time and get paid for completing them!";
+
+export default function Banner() {
+  return(
+    <section className={styles.banner}>
+
+      <section className={styles.content}>
+        <h1 className={styles.message}>{message}</h1>
+        <p className={styles.description}>{description}</p>
+      </section>
+
+      <div className={styles.users_container}>
+        <img src="/images/users.png" alt="users" className={styles.users_img} />
+      </div>
+    </section>
+  );
+}
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import Navbar from '../components/navbar/navbar.jsx'
+import Navbar from '../components/navbar/navbar.jsx';
+import Banner from '../components/banner/banner.jsx';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <Navbar />
+      <Banner />
     </>
   );
 }
