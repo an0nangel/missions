7fa949d5f71a299d97f479a876934c4c9b3d1bcd
angel <89686990+an0nangel@users.noreply.github.com>
2022-07-12 10:39:27 +0200

    add: 'trusted by' bar
    Added a bar that most of websites have, to display who uses this platform. In our case it's just a fake one. It works non the less!

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,11 +1,18 @@
-import Head from 'next/head';
-import Navbar from '../components/navbar/navbar.jsx';
-import Banner from '../components/banner/banner.jsx';
-import styles from '../styles/Home.module.css';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/navbar/navbar.jsx'
+import Banner from '../components/banner/banner.jsx'
+
+// Textual Content
+const content = {
+
+}
+
 
 export default function Home() {
-  return (
-    < >
+  return(
+    <>
       <Head>
         <title>Missions | Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -14,6 +21,32 @@ export default function Home() {
 
       <Navbar />
       <Banner />
+
+      <div className={styles.trustedContainer}>
+        <section className={styles.trusted}>
+          <div>
+            <h2>Trusted by</h2>
+            <h2>the Open Source Community</h2>
+            <div className={styles.rating}>
+              <Image src="/images/star.png" alt="a star for rating" width={44} height={44} />
+              <Image src="/images/star.png" alt="a star for rating" width={44} height={44} />
+              <Image src="/images/star.png" alt="a star for rating" width={44} height={44} />
+              <Image src="/images/star.png" alt="a star for rating" width={44} height={44} />
+              <Image src="/images/star.png" alt="a star for rating" width={44} height={44} />
+            </div>
+          </div>
+
+          <div className={styles.trustedLogos}>
+            <Image src="/images/github.png" alt="GitHub" width={100} height={100} />
+            <Image src="/images/gitlab.png" alt="GitLab" width={100} height={100} />
+          </div>
+        </section>
+      </div>
+
+      { /* Content */ }
+      <main>
+
+      </main>
     </>
   );
 }
